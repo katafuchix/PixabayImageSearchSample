4355d995c32647b8dfe4c07a0a6fb225de345126
katafuchix <katafuchix@gmail.com>
2021-12-31 14:27:37 +0900

    Initial Commit

---
+++ PixabayImageSearchSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PixabayImageSearchSample
+//
+//  Created by cano on 2021/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PixabayImageSearchSample/PixabayImageSearchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixabayImageSearchSampleApp.swift
+//  PixabayImageSearchSample
+//
+//  Created by cano on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct PixabayImageSearchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
